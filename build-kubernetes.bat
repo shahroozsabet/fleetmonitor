a47f6e6ad8cf7fb58204d2ede0a6f0e01ac81092
Shahrooz Sabet <sh.sabet@sadad.co.ir>
2020-01-18 12:58:31 +0330

    build.bat updated.

--- build-kubernetes.bat
+++ build-kubernetes.bat
@@ -2,4 +2,4 @@
 echo '*******************************make sure to install latest integration commons and utils*******************************'
 echo
 echo '*******************************  building for kubernetes environment (All Modules) *******************************'
-call mvn --projects integration-gateway,integration-stream -amd clean install -Pdocker-build -Denvironment=kubernetes
+call mvn --projects integration-gateway,integration-service -amd clean install -Pdocker-build -Denvironment=kubernetes
